c30966da10844877013ea25b572f46f567c2586a
John Wostenberg <jwosty@users.noreply.github.com>
2020-01-08 12:39:56 -0700

    Create windows host and BrowserApp impl

---
+++ HelloWorld.Interstellar/App.fs
@@ -0,0 +1,6 @@
+﻿namespace HelloWorld.Interstellar
+open System
+open Interstellar
+
+module App =
+    let app () = BrowserApp.openAddress (Uri("https://github.com/jwosty/Interstellar"))
---
+++ HelloWorld.Windows/Program.fs
@@ -0,0 +1,21 @@
+﻿namespace HelloWorld.Windows
+open System
+open System.Windows
+open System.Threading
+open Interstellar
+open Interstellar.Chromium.Wpf
+open HelloWorld.Interstellar
+
+type App() =
+    inherit Application(ShutdownMode = ShutdownMode.OnExplicitShutdown)
+
+    override this.OnStartup e =
+        base.OnStartup e
+        BrowserApp.run (HelloWorld.Interstellar.App.app ())
+
+module Main =
+    [<EntryPoint; STAThread>]
+    let main argv =
+        Thread.CurrentThread.Name <- "Main"
+        Interstellar.Chromium.Platform.Initialize ()
+        (new App()).Run ()
